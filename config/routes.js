f17b05bf4af9f2c9ebe2194ed5ac670812d2e256
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-01-11 13:41:37 -0700

    api calls work properly

--- config/routes.js
+++ config/routes.js
@@ -6,22 +6,27 @@ const request = require('request');
 //express' router function
 const router = express.Router();
 
+//starts express application
+const app = express();
+
+//body parsers
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 //requires api keys
 const key = require('../env.js').key;
 const pubKey = require('../env.js').pubKey;
 
-
-
-
-
-
+//Hardcoded seed data
 var team = {
 	name : 'superheroes',
 	number : 10
 };
+
 //Index
 router.get('/api', function searchMarvel (req, res){
-	console.log(req.body);
+	// console.log(req.query.searchName);
 	// console.log('key is ', key);
 	// console.log('pubKey is ', pubKey);
 
@@ -34,11 +39,14 @@ router.get('/api', function searchMarvel (req, res){
 	console.log(hash);
 
 	//api url
-	var apiUrl = 'https://gateway.marvel.com:443/v1/public/characters?name=Thor&ts=' + timeStamp + '&apikey=' + pubKey + '&hash=' + hash;
-	console.log(apiUrl);
-	res.json(team);
-	request(apiUrl, function(err, res, body){
-		// console.log(body);
+	var apiUrl = 'https://gateway.marvel.com:443/v1/public/characters?name=' + req.query.searchName + '&ts=' + timeStamp + '&apikey=' + pubKey + '&hash=' + hash;
+	// console.log(apiUrl);
+	// res.json(team);
+	request(apiUrl, function(err, response, body){
+		// console.log(typeof(body));
+		var results = JSON.parse(body);
+		// console.log(typeof(results));
+		console.log(results.data.results[0].name);
 	});
 });
 
