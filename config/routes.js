b1cf3f229203600d4e9301bacc2d957165a8789e
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-01-11 19:12:22 -0700

    heroku issues 2

--- config/routes.js
+++ config/routes.js
@@ -20,10 +20,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//sets main endpoint to display index.html
-router.route('/', function homepage (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-});
+
 
 //GET request route for Marvel API
 router.route('/search')
